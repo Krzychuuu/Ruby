9f637dc3da0503c0a0b0296d5357e67b7ca1c959
Krzysztof Ochędzan <jurasik@tlen.pl>
2014-04-01 20:34:08 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,49 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Gist.create(
+snippet:
+"
+#include<stdio.h>
+ 
+main()
+{
+   int n;
+ 
+   printf("Enter an integer\n");
+   scanf("%d",&n);
+ 
+   if ( n%2 == 0 )
+      printf("Even\n");
+   else
+      printf("Odd\n");
+ 
+   return 0;
+}
+",
+lang: 'C',
+description: 'Odd or even'
+)
+# ---------------------------------------------
+snippet:
+"
+Program Sample_Text;
+
+{$APPTYPE CONSOLE}
+
+Begin
+  WriteLn('Sample text');
+End.
+",
+lang: 'delphi',
+description: 'Sample text'
+)# ---------------------------------------------
+Gist.create(
+snippet:
+"
+<?php echo "Sample text!"; ?>
+",
+lang: 'php',
+description: 'Sample text'
+)
+
