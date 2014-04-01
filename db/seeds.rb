653af099142ef85e5b2f4729ed45a3bd82fa3045
Krzysztof Ochędzan <jurasik@tlen.pl>
2014-04-01 19:55:43 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,122 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Gist.create(
+snippet:
+"
+#include<stdio.h>
+ 
+main()
+{
+   int n;
+ 
+   printf("Enter an integer\n");
+   scanf("%d",&n);
+ 
+   if ( n%2 == 0 )
+      printf("Even\n");
+   else
+      printf("Odd\n");
+ 
+   return 0;
+}
+",
+lang: 'C',
+description: 'Odd or even'
+)
+# ---------------------------------------------
+Gist.create(
+snippet:
+"
+Program Hello_World;
+
+{$APPTYPE CONSOLE}
+
+Begin
+  WriteLn('Hello World');
+End.
+",
+lang: 'delphi',
+description: 'Hello World w Delphi'
+)
+# ---------------------------------------------
+Gist.create(
+snippet:
+"
+Program Hello_World;
+
+{$APPTYPE CONSOLE}
+
+Begin
+  WriteLn('Hello World');
+End.
+",
+lang: 'delphi',
+description: 'Hello World w Delphi'
+)# ---------------------------------------------
+Gist.create(
+snippet:
+"
+Program Hello_World;
+
+{$APPTYPE CONSOLE}
+
+Begin
+  WriteLn('Hello World');
+End.
+",
+lang: 'delphi',
+description: 'Hello World w Delphi'
+)# ---------------------------------------------
+Gist.create(
+snippet:
+"
+Program Hello_World;
+
+{$APPTYPE CONSOLE}
+
+Begin
+  WriteLn('Hello World');
+End.
+",
+lang: 'delphi',
+description: 'Hello World w Delphi'
+)# ---------------------------------------------
+Gist.create(
+snippet:
+"
+Program Hello_World;
+
+{$APPTYPE CONSOLE}
+
+Begin
+  WriteLn('Hello World');
+End.
+",
+lang: 'delphi',
+description: 'Hello World w Delphi'
+)# ---------------------------------------------
+Gist.create(
+snippet:
+"
+Program Sample_Text;
+
+{$APPTYPE CONSOLE}
+
+Begin
+  WriteLn('Sample text');
+End.
+",
+lang: 'delphi',
+description: 'Sample text'
+)# ---------------------------------------------
+Gist.create(
+snippet:
+"
+<?php echo "Sample text!"; ?>
+",
+lang: 'php',
+description: 'Sample text'
+)
+
